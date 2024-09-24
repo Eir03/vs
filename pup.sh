ba4fc1eb0c9864a0805e2a4f4cbf7ca8c66c2aab
Eir03 <71316160+Eir03@users.noreply.github.com>
2024-09-24 14:43:14 +0300

    Update pup.sh

--- pup.sh
+++ pup.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add –
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt-get install code
